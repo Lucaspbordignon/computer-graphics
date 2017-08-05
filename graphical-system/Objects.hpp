c9bfe4df73334ce1f0ae43b740da4181dd3df431
Lucas <lucaspbordignon99@gmail.com>
2017-08-05 13:43:37 -0300

    Objects class for each type

---
+++ graphical-system/Objects.hpp
@@ -0,0 +1,61 @@
+#ifndef OBJECTS_HPP
+#define OBJECTS_HPP
+
+#include <string>
+
+enum OBJECT_TYPE
+{
+    LINE,
+    POLYGON,
+    POINT
+};
+
+struct Coordinate
+{
+    float x, y;
+    Coordinate(float x, float y): x(x), y(y) {}
+};
+
+class Object
+{
+    public:
+        Object(std::string name, OBJECT_TYPE type, float x, float y):
+            _name(name),
+            _type(type),
+            _coordinate(Coordinate(x, y)) {}
+        virtual ~Object() {};
+        virtual OBJECT_TYPE get_type() {return _type;}
+        std::string get_name() {return _name;}
+        Coordinate get_coordinate() {return _coordinate;}
+
+    private:
+        std::string _name;
+        OBJECT_TYPE _type;
+        Coordinate _coordinate;
+};
+
+class Line: public Object
+{
+    public:
+        Line(std::string name, OBJECT_TYPE type, float x, float y):
+            Object(name, type, x, y) {}
+        ~Line() {}
+};
+
+class Polygon: public Object
+{
+    public:
+        Polygon(std::string name, OBJECT_TYPE type, float x, float y):
+            Object(name, type, x, y) {}
+        ~Polygon() {}
+};
+
+class Point: public Object
+{
+    public:
+        Point(std::string name, OBJECT_TYPE type, float x, float y):
+            Object(name, type, x, y) {}
+        ~Point() {}
+};
+
+#endif // OBJECTS_HPP
